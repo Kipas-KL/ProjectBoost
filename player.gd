60374bf7d0659e8da6e30628636773f51d0df3c6
Kipas-KL <kunzluca662@gmail.com>
2024-07-06 13:26:04 +0200

    First Push

---
+++ player.gd
@@ -0,0 +1,55 @@
+extends RigidBody3D
+
+## How much vertical force to apply when moving.
+@export_range(750.0, 2500.0) var thrust: float = 1000.0
+## How fast rotate the object.
+@export var torque_thrust: float = 100.0
+
+var is_transitioning: bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
+			
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+		
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if is_transitioning == false:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+			
+		if "Hazard" in body.get_groups():
+			crash_sequence()
+		
+func crash_sequence() -> void:
+	print("Kaboom!")
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
+	
+func complete_level(next_level_file: String) -> void:
+	print("Level Complete")
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(
+		get_tree().change_scene_to_file.bind(next_level_file)
+	)
+		
+		
+		
+		
+		
+		
+		
+		
+		
