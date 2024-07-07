2397593151ad51f55e4ffbadfe44ae7a960f3e0f
Kipas-KL <kunzluca662@gmail.com>
2024-07-07 03:18:44 +0200

    Update2
    Add Audio, Add Background, Add Particles, Add Shape to Ship

--- moving_hazard.gd
+++ moving_hazard.gd
@@ -12,6 +12,4 @@ func _ready() -> void:
 	tween.tween_property(self, "global_position", global_position, duration)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
--- player.gd
+++ player.gd
@@ -7,16 +7,30 @@ extends RigidBody3D
 
 var is_transitioning: bool = false
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		%BoosterParticles.emitting = true
+		if %RocketAudio.playing == false:
+			%RocketAudio.play()
+	else: 
+		%BoosterParticles.emitting = false 
+		%RocketAudio.stop()
 			
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+		%RightBoosterParticles.emitting = true
+	else:
+		%RightBoosterParticles.emitting = false
 		
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+		%LeftBoosterParticles.emitting = true
+	else:
+		%LeftBoosterParticles.emitting = false
 
 
 func _on_body_entered(body: Node) -> void:
@@ -29,18 +43,30 @@ func _on_body_entered(body: Node) -> void:
 		
 func crash_sequence() -> void:
 	print("Kaboom!")
+	%ExplosionAudio.play()
+	%BoosterParticles.emitting = false
+	%RightBoosterParticles.emitting = false
+	%LeftBoosterParticles.emitting = false
+	%ExplosionParticles.emitting = true
+	%RocketAudio.stop()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 	
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete")
+	%SuccessAudio.play()
+	%BoosterParticles.emitting = false
+	%RightBoosterParticles.emitting = false
+	%LeftBoosterParticles.emitting = false
+	%SuccessParticles.emitting = true
+	%RocketAudio.stop()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_file)
 	)
